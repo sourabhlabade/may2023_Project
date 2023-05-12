c1c459d28d754d4f53ac5b1ddf39bf22bbc36c9a
Admin <sourabhlabade1995@gmail.com>
2023-05-12 14:41:42 +0530

    Commit 4

--- May_GitHub/src/Sample/Demo.java
+++ May_GitHub/src/Sample/Demo.java
@@ -4,7 +4,7 @@ public class Demo
 {
      public static void main(String[] args) 
      {
+    	  int a=10;
 		  System.out.println("This is My first GitHub Push");
-		  System.out.println("Hi");
      }
 }
